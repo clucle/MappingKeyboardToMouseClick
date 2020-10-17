be558ff1c38954c466aca6931bd53ab64116cabe
clucle <wjdenwls123@gmail.com>
2020-10-17 17:07:11 +0900

    feat: print current cursor position
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <windows.h>
 
-void help() {
+void printHelp() {
 	std::cout << "忙式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式忖\n";
 	std::cout << "弛    MappingKeyboardToMouseClick    弛\n";
 	std::cout << "弛 1. Print Cursor Position          弛\n";
@@ -12,7 +12,33 @@ void help() {
 	std::cout << "戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n";
 }
 
+int input() {
+	int ret = 0;
+	std::cout << " select ( 1 ~ 4 ) : "; std::cin >> ret;
+	if ( ret < 1 || ret > 4 ) {
+		std::cout << " You should select 1 ~ 4\n";
+		return 0;
+	}
+	return ret;
+}
+
 int main() {
 	
-	help();
+	printHelp();
+
+	while( true )
+	{
+		// Get Input
+		int op = input();
+		if ( !op ) continue;
+		system( "CLS" );
+		printHelp();
+
+		if ( op == 1 ) {
+			POINT cursor;
+			GetCursorPos( &cursor );
+			std::cout << " Current Cursor Position \n";
+			std::cout << "  - x : " << cursor.x << " y : " << cursor.y << '\n';
+		}
+	}
 }
