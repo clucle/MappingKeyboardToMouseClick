cbab6339155fc0d28df8ba31aa258bc51818e809
clucle <wjdenwls123@gmail.com>
2020-10-17 16:56:41 +0900

    feat: print help
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 
 #include <iostream>
+#include <windows.h>
+
+void help() {
+	std::cout << "忙式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式忖\n";
+	std::cout << "弛    MappingKeyboardToMouseClick    弛\n";
+	std::cout << "弛 1. Print Cursor Position          弛\n";
+	std::cout << "弛 2. Mapping Keyboard To Mouseclick 弛\n";
+	std::cout << "弛 3. Show Mapping List              弛\n";
+	std::cout << "弛 4. Run                            弛\n";
+	std::cout << "戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n";
+}
 
 int main() {
-	std::cout << "Hello World!\n";
+	
+	help();
 }
